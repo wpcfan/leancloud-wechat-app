c04a1359101495c7ea71b8cfcf091ad78b6fb85a
Peng Wang <wpcfan@gmail.com>
2022-09-28 13:44:04 +0800

    update app

--- app.py
+++ app.py
@@ -136,3 +136,28 @@ def all(**kwargs):
     监听所有没有更特殊的事件
     """
     return msg.reply(kwargs['sender'], sender=kwargs['receiver'], content='all')
+
+
+@msg.text()
+def hello(**kwargs):
+    """
+    监听所有文本消息
+    """
+    return "hello too"
+
+
+@msg.text("help")
+def world(**kwargs):
+    """
+    监听help消息
+    """
+    return dict(content="hello world!")
+
+
+@msg.subscribe
+def subscribe(**kwargs):
+    """
+    监听订阅消息
+    """
+    print(kwargs)
+    return "欢迎订阅我们的公众号"
